1d6e5e6e878b4a18a9a71c15db53b76c8be2002a
BaSO-4 <luka.sabotic@gmail.com>
2025-06-06 18:04:00 +0200

    fixed save path

--- data/data.py
+++ data/data.py
@@ -1,10 +1,10 @@
 from torchvision.datasets import Flowers102
 from torchvision import transforms
 
-def prepare_flowers102(root_dir, output_dir_name="flowers102_5k", sample_size=5000):
+def prepare_flowers102(root_dir):
     print(f"Downloading Oxford 102 Flowers into {root_dir}/flowers102/jpg/ …")
     Flowers102(root=root_dir, split="train", download=True, transform=transforms.ToTensor())
     print("Done.")
 
 if __name__ == "__main__":
-    prepare_flowers102(".\\data")
+    prepare_flowers102("./data")
